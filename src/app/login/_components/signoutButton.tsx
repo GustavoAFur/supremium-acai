b5f37626cba1e6c33a7e8319d09c470d651f5b39
Gustavo <gustavoaragaof@gmail.com>
2025-01-14 19:47:40 -0300

    feedback de carregamento do botao sair

--- src/app/login/_components/signoutButton.tsx
+++ src/app/login/_components/signoutButton.tsx
@@ -5,20 +5,34 @@ import { signOut } from "firebase/auth";
 import { auth } from "@/utils/firebaseConfig";
 import { deleteCookie } from "cookies-next";
 import { Button } from "@/components/ui/button"; // Importe o botão
+import { useState } from "react";
 
 export default function SignOutButton() {
   const router = useRouter(); // hook que só funciona em Client Component
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleSignOut = async () => {
-    // Desconecta o usuário do Firebase
-    await signOut(auth);
+    setIsLoading(true);
+    try {
+      // Desconecta o usuário do Firebase
+      await signOut(auth);
 
-    // Remove o token do cookie
-    deleteCookie("access_token");
+      // Remove o token do cookie
+      deleteCookie("access_token");
 
-    // Redireciona para a página de login
-    router.push("/login");
+      // Redireciona para a página de login
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  return <Button onClick={handleSignOut}>Sair</Button>;
+  return (
+    <Button disabled={isLoading} onClick={handleSignOut}>
+      {isLoading ? "Saindo" : "Sair"}
+    </Button>
+  );
 }
