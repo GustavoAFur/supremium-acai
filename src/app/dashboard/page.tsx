09f990a1238075614cde4c573f291682f64c739c
Gustavo <gustavoaragaof@gmail.com>
2025-01-17 20:20:28 -0300

    adicionado a logo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Cantina</h1>
+    <div className="relative flex items-center justify-center min-h-screen">
+      <Image
+        src={"/logo.png"}
+        alt="Logo"
+        width={300}
+        height={300}
+        objectFit="contain"
+      />
     </div>
   );
 }
